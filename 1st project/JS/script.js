23068b1e3c7017283ba4bf044dba09d63bf26189
Egor Kovalev <egor.kovalev@astrazeneca.com>
2021-06-02 18:06:22 +0300

    Done 3rd homework

--- 1st project/JS/script.js
+++ 1st project/JS/script.js
@@ -1,17 +1,16 @@
-"use strict";
+'use strict';
 
-/* Задание на урок:
-1) Автоматизировать вопросы пользователю про фильмы при помощи цикла
-2) Сделать так, чтобы пользователь не мог оставить ответ в виде пустой строки,
-отменить ответ или ввести название фильма длинее, чем 50 символов. Если это происходит - 
-возвращаем пользователя к вопросам опять. (К любой строке можно обратиться как 
-str.length - и получить её длину)
-3) При помощи условий проверить  personalMovieDB.count, и если он меньше 10 - вывести сообщение
-"Просмотрено довольно мало фильмов", если от 10 до 30 - "Вы классический зритель", а если больше - 
-"Вы киноман". А если не подошло ни к одному варианту - "Произошла ошибка"
-4) Потренироваться и переписать цикл еще двумя способами*/
+let numberOfFilms;
 
-const numberOfFilms = +prompt("How much movies have you already seen","");
+function start() {
+    numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+
+    while (numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)) {
+        numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+    }
+}
+
+start();
 
 const personalMovieDB = {
     count: numberOfFilms,
@@ -19,29 +18,52 @@ const personalMovieDB = {
     actors: {},
     genres: [],
     privat: false
-
 };
 
-if (personalMovieDB.count < 10) {
-    alert("You have seen pretty small number of films");
-} else if (personalMovieDB.count >= 10 && personalMovieDB.count <= 30) {
-    alert("You're a classical watcher");
-} else if (personalMovieDB > 30) {
-    alert("You're a movie fan");
-} else {
-    alert('Error');
-}
 
-for (let i = 0; i < 2; i++) {
-    let oneOfLastSeenMovies = prompt('One of the last movies you have aready seen?','');
-    let movieRating = +prompt(`How much stars from 0 to 10 ${oneOfLastSeenMovies} has been?`,'');
-
-    if (oneOfLastSeenMovies != null && movieRating != null && oneOfLastSeenMovies != '' && movieRating != '' && oneOfLastSeenMovies.length < 50 && movieRating.length < 50 ) {
-        personalMovieDB.movies[oneOfLastSeenMovies] = movieRating;
-    } else {
-        i--;
+function rememberMyFilms() {
+    for (let i = 0; i < 2; i++) {
+        const a = prompt('Один из последних просмотренных фильмов?', ''),
+              b = prompt('На сколько оцените его?', '');
+    
+        if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+            personalMovieDB.movies[a] = b;
+            console.log('done');
+        } else {
+            console.log('error');
+            i--;
+        }
     }
-     
 }
 
-console.log(personalMovieDB);
+rememberMyFilms();
+
+function detectPersonalLevel() {
+    if (personalMovieDB.count < 10) {
+        console.log("Просмотрено довольно мало фильмов");
+    } else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+        console.log("Вы классический зритель");
+    } else if (personalMovieDB.count >= 30) {
+        console.log("Вы киноман");
+    } else {
+        console.log("Произошла ошибка");
+    }
+}
+
+detectPersonalLevel();
+
+function showMyDB (hidden) {
+    if (!hidden) {
+        console.log(personalMovieDB);
+    }
+}
+
+showMyDB(personalMovieDB.privat);
+
+function writeYourGenres() {
+    for (let i = 1; i <= 3; i++) {
+        personalMovieDB.genres[i - 1] = prompt(`Ваш любимый жанр под номером ${i}`);
+    }
+}
+
+writeYourGenres();
--- 1st project/JS/test script.js
+++ 1st project/JS/test script.js
@@ -1,8 +1,2 @@
 "use strict";
 
-let num = 50;
-
-while(num < 55) {
-    console.log(num);
-    num++;
-}
